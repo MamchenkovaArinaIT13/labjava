4871db03349c5b3ba1f6363f82cc112014409e19
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-09-28 20:05:30 +0500

    Create Employee.java

---
+++ JavaLab2/Employee.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+public class Employee {
+
+    // Поля
+    private String name;
+    private Departament departament;
+
+
+    //Геттеры и сеттеры
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Конструкторы
+    public Employee() {
+        this.name = null;
+        this.departament = null;
+    }
+
+    public Employee(String name, Departament departament) {
+        this.name = name;
+        this.departament = departament;
+        if (departament != null) {
+            departament.addEmployee(this);
+        }
+    }
+
+    //Метод to_string
+    @Override
+    public String toString() {
+        String managerName = (departament != null && departament.getManager() != null)
+                ? departament.getManager().getName() : "не назначен";
+        String departmentName = (departament != null) ? departament.getName1() : "не указан";
+
+        return this.name + " работает в отделе " + departmentName
+                + ", начальник которого " + managerName;
+    }
+
+    // Метод для получение всех сотрудников по ссылке
+    public List<Employee> getAllDepartmentEmployees() {
+        return departament.getEmployees();
+    }
+}
