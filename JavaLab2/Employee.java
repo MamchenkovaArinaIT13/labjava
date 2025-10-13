faf9e91d6f49af10f1e198cc448a3d282c727459
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-10-13 08:03:47 +0300

    Update Employee.java

--- JavaLab2/Employee.java
+++ JavaLab2/Employee.java
@@ -2,47 +2,46 @@ import java.util.List;
 
 public class Employee {
 
-    // Поля
-    private String name;
-    private Departament departament;
+  // Поля
+  private String name;
+  private Departament departament;
+  
+  //Конструкторы
+  public Employee() {
+    this.name = null;
+    this.departament = null;
+  }
 
-
-    //Геттеры и сеттеры
-    public String getName() {
-        return this.name;
+  public Employee(String name, Departament departament) {
+    this.name = name;
+    this.departament = departament;
+    if (departament != null) {
+      departament.addEmployee(this);
     }
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  //Геттеры и сеттеры
+  public String getName() {
+    return this.name;
+  }
 
-    //Конструкторы
-    public Employee() {
-        this.name = null;
-        this.departament = null;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  //Метод to_string
+  @Override
+  public String toString() {
+    String managerName = (departament != null && departament.getManager() != null)
+            ? departament.getManager().getName() : "не назначен";
+    String departmentName = (departament != null) ? departament.getName1() : "не указан";
 
-    public Employee(String name, Departament departament) {
-        this.name = name;
-        this.departament = departament;
-        if (departament != null) {
-            departament.addEmployee(this);
-        }
-    }
+    return this.name + " работает в отделе " + departmentName
+            + ", начальник которого " + managerName;
+  }
 
-    //Метод to_string
-    @Override
-    public String toString() {
-        String managerName = (departament != null && departament.getManager() != null)
-                ? departament.getManager().getName() : "не назначен";
-        String departmentName = (departament != null) ? departament.getName1() : "не указан";
-
-        return this.name + " работает в отделе " + departmentName
-                + ", начальник которого " + managerName;
-    }
-
-    // Метод для получение всех сотрудников по ссылке
-    public List<Employee> getAllDepartmentEmployees() {
-        return departament.getEmployees();
-    }
+  // Метод для получение всех сотрудников по ссылке
+  public List<Employee> getAllDepartmentEmployees() {
+    return departament.getEmployees();
+  }
 }
