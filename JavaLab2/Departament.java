5e3a329db81f89fd0ae3d2f0392565f7311786a7
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-10-13 08:02:52 +0300

    Update Departament.java

--- JavaLab2/Departament.java
+++ JavaLab2/Departament.java
@@ -3,51 +3,50 @@ import java.util.List;
 
 public class Departament {
 
-    // Поля
-    private String name1;
-    private Employee manager;
-    private List<Employee> employees;
+  // Поля
+  private String name1;
+  private Employee manager;
+  private List<Employee> employees;
 
+  //Конструктор
+  public Departament() {
+    this.name1 = null;
+    this.manager = null;
+    this.employees = null;
+  }
 
-    //Геттеры и сеттеры
-    public String getName1() {
-        return this.name1;
+  public Departament(String name1) {
+    this.name1 = name1;
+    this.manager = null;
+    this.employees = new ArrayList<>();
+  }
+
+  //Геттеры и сеттеры
+  public String getName1() {
+    return this.name1;
+  }
+
+  public Employee getManager() {
+    return this.manager;
+  }
+
+  public void setManager(Employee manager) {
+    this.manager = manager;
+  }
+
+  //Метод to_string
+  @Override
+  public String toString() {
+    return this.name1;
+  }
+
+  public void addEmployee(Employee employee) {
+    if (!employees.contains(employee)) {
+      employees.add(employee);
     }
+  }
 
-    public Employee getManager() {
-        return this.manager;
-    }
-
-    public void setManager(Employee manager) {
-        this.manager = manager;
-    }
-
-    //Конструктор
-    public Departament() {
-        this.name1 = null;
-        this.manager = null;
-        this.employees = null;
-    }
-
-    public Departament(String name1) {
-        this.name1 = name1;
-        this.manager = null;
-        this.employees = new ArrayList<>();
-    }
-
-    //Метод to_string
-    @Override
-    public String toString() {
-        return this.name1;
-    }
-
-    public void addEmployee(Employee employee) {
-        if (!employees.contains(employee)) {
-            employees.add(employee);
-        }
-    }
-
-    public List<Employee> getEmployees() {
-        return new ArrayList<>(employees);
-    }
+  public List<Employee> getEmployees() {
+    return new ArrayList<>(employees);
+  }
 }
